ae12ca89a374be25abd4adc86dd1c2df1f3fa812
Marlon Jones <marlonjones234@gmail.com>
2016-09-01 12:38:44 -0500

    Start of Permissions

--- app/src/main/java/com/marlonjones/projectevo/activity/ActivityHome.java
+++ app/src/main/java/com/marlonjones/projectevo/activity/ActivityHome.java
@@ -9,16 +9,26 @@ import com.marlonjones.projectevo.model.SlideData;
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnMenuTabClickListener;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.IdRes;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 public class ActivityHome extends AppCompatActivity{
@@ -40,8 +50,11 @@ public class ActivityHome extends AppCompatActivity{
         toolbar.setTitle("Aperture Gallery");
         setSupportActionBar(toolbar);
 
-        mContext=ActivityHome.this;
+        //Permission Check Start
 
+        //Permission Check End
+
+        mContext=ActivityHome.this;
 		mBottomBar = BottomBar.attach(this, savedInstanceState);
         mBottomBar.noTabletGoodness();
 		mBottomBar.setItems(R.menu.bottombar_menu);
