7843b51179690474aaaa01ce307d46e5d982c44a
Marlon Jones <marlonjones234@gmail.com>
2017-04-11 10:42:28 -0500

    Gradle, String, and Manifest changes

--- app/build/generated/source/buildConfig/androidTest/debug/com/marlonjones/projectevo/test/BuildConfig.java
+++ app/build/generated/source/buildConfig/androidTest/debug/com/marlonjones/projectevo/test/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.marlonjones.projectevo.test";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 3;
-  public static final String VERSION_NAME = "0.0.7 Unreleased";
+  public static final int VERSION_CODE = 12;
+  public static final String VERSION_NAME = "0.0.8-A";
 }
